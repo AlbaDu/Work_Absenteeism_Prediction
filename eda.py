5c93e2a6a0e7b369f2676d6f1042ae34a4ce51e0
Alba Durán Merinero <duran.merinero@gmail.com>
2020-09-28 03:03:31 +0200

    carga de archivos

---
+++ eda.py
@@ -0,0 +1,61 @@
+#Import the required libraries
+import pandas as pd
+import numpy as np
+
+
+def file2df(file_):
+    """
+    ---What it does---
+        + Generates a dataframe from a files: ".csv", ".xlsx" or ".xls".
+    ---What it needs---
+        + file: dataframe with columns.
+
+    IMPORTANT! the files need to be stored in the path specified by the function.    
+    ---What it returns---
+        + new_df: dataframe containing the data from the file.
+    """
+    path = "C:\\Users\\34609\\Documents\\Repos Git\\Data\\"
+    
+    if file_.endswith("csv"):
+        name = str(path + file_)
+        new_df = pd.read_csv(name)
+        return new_df
+    elif file_.endswith("xlsx") or file_.endswith("xls"): 
+        name = str(path + file_)
+        new_df = pd.read_excel(name)
+        return new_df
+
+def date2weekday(date):
+    """
+    ---What it does---
+        + Gives the weekday corresponding with a certain date.
+    ---What it needs---
+        + A date entry with datetime type.
+    ---What it returns---
+        + A value from 0 to 6, corresponding to the weekdays from Monday to Sunday.
+    """
+    return date.weekday()
+
+def date2month(date):
+    """
+    ---What it does---
+        + Gives the month corresponding with a certain date.
+    ---What it needs---
+        + A date entry with datetime type.
+    ---What it returns---
+        + A value from 0 to 12, corresponding to the months from January to December.
+    """
+    return date.month
+
+
+def df2file(df_):
+    """
+    ---What it does---
+        + Generates a .csv file from a df.
+    ---What it needs---
+        + df: containing data in rows and columns. 
+    IMPORTANT! the files would be stored in the path specified by the function and the name should be given WITHOUT the .csv extension.
+    """
+    name = input("Name of the file WITHOUT .csv")
+    path = str("C:\\Users\\34609\\Documents\\Repos Git\\Data\\" + name + ".csv")
+    df_.to_csv(path, index = False)
---
+++ ml.py
@@ -0,0 +1,32 @@
+#Import the required libraries
+import pandas as pd
+import numpy as np
+
+def file2df(file_):
+    """
+    ---What it does---
+        + Generates a dataframe from a .csv file.
+    ---What it needs---
+        + file: dataframe with columns.
+
+    IMPORTANT! the files need to be stored in the path specified by the function.    
+    ---What it returns---
+        + new_df: dataframe containing the data from the file.
+    """
+    path = "C:\\Users\\34609\\Documents\\Repos Git\\Data\\"
+    name = str(path + file_)
+    new_df = pd.read_csv(name)
+    return new_df
+
+def target(parameter, condition_):
+    """
+    ---What it does---
+        + Generates the value of the target (0 | 1) depending on the condition value.
+    ---What it needs---
+        + condition: value of a parameter.   
+    ---What it returns---
+        + target: numerical character with value 0 or 1, that classifies the df record according to a pre-set condition.
+    """
+    value = np.where(parameter > condition_, 1, 0)
+    return value          
+   
